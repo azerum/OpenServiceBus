c95d5d1c59ad5fec60e7873cfc27a06f09649951
MauritsArissen <marissen1@live.nl>
2026-05-18 08:38:29 +0200

    feat: Refactor and implement architecture to projects

--- src/OpenServiceBus.Abstractions/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("OpenServiceBus.Core")]
-[assembly: InternalsVisibleTo("OpenServiceBus.Broker")]
-[assembly: InternalsVisibleTo("OpenServiceBus.Amqp")]
-[assembly: InternalsVisibleTo("OpenServiceBus.Management")]
